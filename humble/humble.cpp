f14fe203eb4d11eabc7f9afbc2d298fb852ee6fb
guanglightman <guanglightman@gmail.com>
2016-03-23 20:02:17 +0800

    typical trick

---
+++ humble/humble.cpp
@@ -0,0 +1,49 @@
+/*
+ID: guangli1
+PROG: humble
+LANG: C++11  
+*/
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <algorithm>
+#include <utility>
+#include <cmath>
+#include <cstring>
+#define LL unsigned long long
+#define inf 
+#define INF (~(1<<31))
+using namespace std;
+const int N = 10;
+const int M = 100;
+int p[N];
+int num[M];
+int h[N] = {0};
+int n, m;
+int main(){
+    freopen("humble.out", "w", stdout);
+    freopen("humble.in", "r", stdin);
+    //cin >> n >> m;
+    scanf("%d %d", &n, &m);
+    for(int i = 0; i < n; i++)
+        //cin >> p[i];
+        scanf("%d", &p[i]);
+    int l = 0;
+    num[l] = 1;
+    while(l < m){
+        ++l; 
+        int a = INF;
+        for(int i = 0; i < n; i++){
+            while(num[h[i]]*p[i] <= num[l-1])
+                h[i]++;
+            a = min(a, num[h[i]]*p[i]);
+        }
+        num[l] = a; 
+    } 
+    cout << num[m] << endl;
+    return 0;
+}
