2fd36347629936b0487c38125f9dff68f6dcaaa8
guanglightman <guanglightman@gmail.com>
2016-03-23 20:06:59 +0800

    slightly modify

--- humble/humble.cpp
+++ humble/humble.cpp
@@ -18,8 +18,8 @@ LANG: C++11
 #define inf 
 #define INF (~(1<<31))
 using namespace std;
-const int N = 10;
-const int M = 100;
+const int N = 105;
+const int M = 100005;
 int p[N];
 int num[M];
 int h[N] = {0};
@@ -27,11 +27,11 @@ int n, m;
 int main(){
     freopen("humble.out", "w", stdout);
     freopen("humble.in", "r", stdin);
-    //cin >> n >> m;
-    scanf("%d %d", &n, &m);
+    cin >> n >> m;
+    //scanf("%d %d", &n, &m);
     for(int i = 0; i < n; i++)
-        //cin >> p[i];
-        scanf("%d", &p[i]);
+       cin >> p[i];
+       // scanf("%d", &p[i]);
     int l = 0;
     num[l] = 1;
     while(l < m){
